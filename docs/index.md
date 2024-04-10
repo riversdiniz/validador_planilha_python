a27ab49453c56cb8881c1c04b79ba89c541a975d
River Diniz <riverd@ciandt.com>
2024-04-10 16:51:28 -0300

    Finalizando o codigo em python

---
+++ docs/index.md
@@ -0,0 +1,5 @@
+# Projeto de validação do arquivo em Excel
+
+## Esse é meu contato de dados
+
+::: contrato_de_dados.InventarioSchema
--- main.py
+++ main.py
@@ -1,15 +1,38 @@
 import pandas as pd
-
 from pandas import DataFrame
-
 from contrato_de_dados import InventarioSchema
 
-minha_tabela: DataFrame = pd.read_excel("data\inventario_mundo.xlsx")
-
-print(minha_tabela)
-
-try:
-    InventarioSchema.validate(minha_tabela)
-    print("Excel esta bom!")
-except Exception as e:
-    print(f"Teve erro de schema: {e}")
+import streamlit as st
+import sqlite3
+  
+def main():
+    st.title("Validador de Inventário")
+    
+    # Widget para upload do arquivo Excel
+    uploaded_file = st.file_uploader("Escolha um arquivo de Excel", type=["xlsx"])
+    if uploaded_file is not None:
+        
+        # Lendo o arquivo Excel
+        df_para_validar = pd.read_excel(uploaded_file)
+        
+        # Tentativa de validação do DataFrame com o esquema
+        
+        try:
+            new_df = InventarioSchema.validate(df_para_validar)
+            st.write(new_df)
+            st.success("Validação do esquema bem-sucedida!")
+        # Botão para salvar no banco de dados
+        
+            if st.button("Salvar no Banco de Dados"):
+                
+            # Criando uma conexão com o banco de dados SQLite
+                con = sqlite3.connect('inventario.db')
+                df_para_validar.to_sql("inventario", con, if_exists='replace', index=False)
+                st.success("Dados salvos com sucesso no banco de dados!")
+                con.close()
+        except Exception as e:
+            st.error(f"Erro de validação: {e}")
+            
+if __name__ == "__main__":
+    main()
+        
